ba5a0c51901e8c536aa38f7e79b7827b27d8b4fe
DUC Nguyen Van 2 (VTI.D10) <duc.nguyenvan2@vti.vn>
2023-09-05 16:52:50 +0700

    add channel k+

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
 $(function(){
     const channels = [
+        {name: "kplus-sport-1", link: "http://b4tv.live/b4tv/b4tv/76.m3u8"},
+        {name: "kplus-sport-2", link: "http://b4tv.live/b4tv/b4tv/86.m3u8"},
         {name: "onfootball", link: "https://live2onsport.vtvcab.vn/dash/BONGDA_HD/m30_index.m3u8"},
         {name: "onsport", link: "https://live2onsport.vtvcab.vn/dash/THETHAO_HD/m40_index.m3u8"},
-        {name: "tv360", link: "https://live-zlr1.tv360.vn/manifest/Su_Kien_3/playlist_1080p.m3u8"},
+        {name: "tv360-1", link: "http://b4tv.live/b4tv/b4tv/3.m3u8"},
+        {name: "tv360-2", link: "https://live-zlr1.tv360.vn/manifest/Su_Kien_4/playlist_1080p.m3u8"},
+        {name: "tv360-3", link: "https://live-zlr1.tv360.vn/manifest/Su_Kien_3/playlist_1080p.m3u8"},
         {name: "onsportplus", link: "https://live2onsport.vtvcab.vn/dash/HAY_TV/m30_index.m3u8"},
         {name: "premiersport2", link: "https://cdn3.skygo.mn/live/disk1/SPSPlay/HLS-FTA/SPSPlay.m3u8"},
     ]
